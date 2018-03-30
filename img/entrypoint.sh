0356b8c308aacc734a757521f6b84ac110ca56bb
Mickael KERJEAN <mickael.kerjean@gmail.com>
2018-03-30 07:26:32 +0200

    proper setup

--- img/entrypoint.sh
+++ img/entrypoint.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
-cd /app/koel
+cd /app/code
 
-# wait for mysql to be up and running
+
+echo "Waiting for Database Initialisation"
 while ! mysqladmin ping -h"$DB_HOST" --silent; do
     sleep 1
 done
 
-# initialise on first run
-if [ -f "/app/init.exp" ]; then
-    #expect /app/init.exp
-    #rm /app/init.exp
-fi
 
-php artisan serve --host 0.0.0.0
+if [ -f "/app/init.exp" ]; then
+    echo "Initialise application"
+    expect /app/init.exp
+
+    # https://github.com/phanan/koel/issues/681
+    if [ $HTTPS == "true" ]; then
+       sed -i '3i$_SERVER["HTTPS"] = "on";' index.php
+    fi
+    rm /app/init.exp
+fi
+chown -R www-data:www-data /app/music/
+
+echo "Everything's ready, starting the server"
+apachectl -DFOREGROUND
 exec $0
